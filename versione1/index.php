44553f8f5beec866d762a0bb9fbe3ba537e3e15d
Fraianni <francescoianni1995@gmail.com>
2022-10-17 19:58:51 +0200

    cards prima versione

--- versione1/index.php
+++ versione1/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.13/vue.min.js" integrity="sha512-9PmLIATEd4w0OxdTHq61p12lQqa8OtDC6nXu0+S1Icb800oxoADle+gaPfmAXLa35KSvk/6bGeNRfi3nq0Hvvw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="../style/style.css">
     <title>Dischi</title>
     <script src="script/script.js" defer></script>
 </head>
@@ -14,9 +15,16 @@
 <body>
     <div id="app">
         <!-- <input type="text" v-model="toSearch" @keyup.enter="filterStudents"> -->
-        <div v-for="(disco, index) of dischi" :key="index">
-            {{disco.title}}
+        <div class="cards">
+            <div class="card" v-for="(disco, index) of dischi" :key="index">
+                <img :src="disco.poster" alt="">
+                <p>{{disco.title}}</p>
+                <p>{{disco.author}}</p>
+                <p>{{disco.genre}}</p>
+
+            </div>
         </div>
+
     </div>
 </body>
 
