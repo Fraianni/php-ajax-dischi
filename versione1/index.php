cdc11573a775675edf3206295ed469eeaa5fccc8
Fraianni <francescoianni1995@gmail.com>
2022-10-18 09:58:20 +0200

    cards versione1 ok

--- versione1/index.php
+++ versione1/index.php
@@ -7,35 +7,33 @@ include __DIR__ .  '/data/data.php';
     foreach ($dischi as $disco) {
     ?>
         <div class="card">
-            <?php
-            echo "<img src='" . $disco['poster'] . "' alt='img'>";
-            echo  $disco['author'];
-            echo $disco['genre'];
-            echo $disco['title'];
-            ?>
+            <img src="<?= $disco['poster'] ?>" alt="<?= $disco['title'] ?>">
+            <p> <?= $disco['author'] ?> </p>
+            <p> <?= $disco['title'] ?> </p>
+            <p> <?= $disco['genre'] ?> </p>
         </div>
+
 
     <?php
     }
     ?>
-</div>
 
 
 
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/style.css">
-    <title>Dischi</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../style/style.css">
+        <title>Dischi</title>
+    </head>
 
-<body>
+    <body>
 
-</body>
+    </body>
 
-</html>
+    </html>
