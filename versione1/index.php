8526a69a8f11321f7a31eff327dd7922ba1fdf13
Fraianni <francescoianni1995@gmail.com>
2022-10-18 09:51:15 +0200

    card versione 1 visibili

--- versione1/index.php
+++ versione1/index.php
@@ -1,6 +1,41 @@
 <?php
 include __DIR__ .  '/data/data.php';
+?>
 
-foreach($dischi as $disco){
-    echo $disco['title'];
-}
+<div class="cards">
+    <?php
+    foreach ($dischi as $disco) {
+    ?>
+        <div class="card">
+            <?php
+            echo "<img src='" . $disco['poster'] . "' alt='img'>";
+            echo  $disco['author'];
+            echo $disco['genre'];
+            echo $disco['title'];
+            ?>
+        </div>
+
+    <?php
+    }
+    ?>
+</div>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Dischi</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
